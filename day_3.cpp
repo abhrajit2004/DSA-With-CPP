b0e793afbc24a9ea37db1a72db1c3d8b937b28f7
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-20 20:43:46 +0530

    initial commit

---
+++ day_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // cout << "Hello Abhrajit" << endl;
+    int a = 10, b = 20;
+    char c = 'p';
+    float f = 2.31;
+    double d = 3.121;
+    bool m = true;
+    cout << m << endl;
+    cout << d << endl;
+    cout << f << endl;
+    cout << c << endl;
+    cout << a << endl;
+    cout << a + b << endl;
+    cout << "a" << endl;
+    cout << "Hello Abhrajit\n";
+    cout << "3+2" << endl;
+    cout << 3 + 2;
+
+    return 0;
+};
