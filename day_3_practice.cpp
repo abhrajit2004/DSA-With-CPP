762bae4e8b18747e3f7ca394186ed81cbf73d0f1
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-20 21:57:40 +0530

    added new file

---
+++ day_3_practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 15, b = 5;
+    cout << "The product of " << a << " and " << b << " is " << a * b << endl;
+    cout << "The subtraction of " << a << " and " << b << " is " << a - b << endl;
+    cout << "6*3";
+    return 0;
+};
