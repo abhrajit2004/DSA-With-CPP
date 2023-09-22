2cc34fe58cbe6ce88986d31a67fe3395422e6d89
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-22 17:18:19 +0530

    changed one line

--- world_cup.cpp
+++ world_cup.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int count = 0;
+    int count;
     for (count = 1; count <= 20; count = count + 1)
     {
         cout << "India will win the World Cup 2023" << endl;
