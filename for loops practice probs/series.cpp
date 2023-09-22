0999bceac18045f976a11a9d23460d30b646bc4f
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-23 01:49:02 +0530

    added new file

---
+++ for loops practice probs/series.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char count;
+    for ( count = 'a'; count <= 'z'; count = count + 1)
+    {
+        cout << count << " ";
+    }
+    return 0;
+}
