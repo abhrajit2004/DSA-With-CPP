43e7db374c59cd003d972a520a8788f5bd73ed8b
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-22 16:40:12 +0530

    Added file

--- if_else.cpp
+++ if_else.cpp
@@ -4,12 +4,26 @@ using namespace std;
 
 int main()
 {
-    int marks;
-    cout << "Enter your marks: ";
-    cin >> marks;
-    if (marks > 33)
-        cout << "Pass";
-    else
-        cout << "Fail";
+    // int marks;
+    // cout << "Enter your marks: ";
+    // cin >> marks;
+    // if (marks > 33)
+    //     cout << "Pass";
+    // else
+    //     cout << "Fail";
+    int a,b;
+    printf("Enter first number: ");
+    cin >> a;
+    printf("Enter second number: ");
+    cin >> b;
+    if(a>b){
+        cout << a << " is greater than " << b;
+    }
+    else if(b>a){
+        cout << b << " is greater than " << a;
+    }
+    else{
+        cout << "Both are equal";
+    }
     return 0;
 }
