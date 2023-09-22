bfd1a02b6aadf9e4a80d78a7fdec16ca5baf9b33
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-22 16:42:36 +0530

    Added new file

---
+++ day_4_practice.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int number;
+    // cout << "Enter a number: ";
+    // cin >> number;
+    // if (number % 2 == 0)
+    // {
+    //     cout << number << " is even";
+    // }
+    // else
+    // {
+    //     cout << number << " is odd";
+    // }
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age > 18)
+    {
+        cout << "You are adult";
+    }
+    else
+    {
+        cout << "You are teenager";
+    }
+}
