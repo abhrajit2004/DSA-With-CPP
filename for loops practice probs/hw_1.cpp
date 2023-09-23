5fe4732efcc7a1dcd4299cc001df5b8f330e1bfb
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-23 17:39:05 +0530

    added new file

---
+++ for loops practice probs/hw_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int count;
+    for (count = 280; count >= 250; count = count - 1)
+    {
+        cout << count << endl;
+    }
+    
+    return 0;
+}
