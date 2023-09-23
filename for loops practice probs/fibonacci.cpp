a2b85a1ba0191230a72e4677251a687c83e10b82
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-23 17:38:33 +0530

    added file

--- for loops practice probs/fibonacci.cpp
+++ for loops practice probs/fibonacci.cpp
@@ -6,14 +6,12 @@ int main(){
     int curr, prev = 1 , last = 0,n,i;
     cout << "Enter the number of terms: ";
     cin >> n;
-    cout << last << " " << prev << " ";
     for (i = 3; i <= n; i = i + 1)
     {
         curr = prev + last;
-        cout << curr << " ";
         last  = prev;
         prev = curr;
     }
-    
+    cout << curr << " ";
     return 0;
 }
