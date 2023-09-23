87fa2a7418cee33991af6bca5e4d796e21aee005
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-09-23 17:40:54 +0530

    changed file

--- for loops practice probs/reverse_series.cpp
+++ for loops practice probs/reverse_series.cpp
@@ -5,16 +5,23 @@ using namespace std;
 int main()
 {
     int n,i;
-    cout << "Enter range: ";
-    cin >> n; 
+    char ch;
+
+    for (ch = 'Z'; ch >= 'A'; ch = ch - 1)
+    {
+        cout << ch << " ";
+    }
+    
+    // cout << "Enter range: ";
+    // cin >> n; 
     // for (i = n; i >= 1; i--)
     // {
     //     cout << i << " ";
     // }
-    for (i = 1; i <= n; i = i + 3)
-    {
-        cout << i << " ";
-    }
+    // for (i = 1; i <= n; i = i + 3)
+    // {
+    //     cout << i << " ";
+    // }
 
     return 0;
 }
